54616b6d22c6d37bb1f201cd076e3c3d2e8ee86c
Off <80608865+ptmo@users.noreply.github.com>
2024-06-09 21:11:36 +0700

    Update swiper.js

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,1 +1,17 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+        delay: 3000, // 3 detik
+        disableOnInteraction: false, // Jangan berhenti saat ada interaksi
+      },
+    });
+  });
